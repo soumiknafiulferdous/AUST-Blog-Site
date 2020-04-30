c364b928ccaede3fc35e787caa546d6e728701d7
Soumik Nafiul Ferdous <nafiul.ferdous@gmail.com>
2020-04-30 20:47:26 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,88 @@
+<?php
+//index.php
+
+?>
+<!DOCTYPE html>
+<html>
+ <head>
+  <title>AUST BLOG</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+ </head>
+ <body>
+  <br />
+  <h2 align="center"><a href="#">AUST BLOG</a></h2>
+    <h3 align="left"><a href="#">Writer: Soumik </a></h3>
+	    <h4 align="left"><a href="#">Date: 31/1/2018 </a></h4>
+	
+  <br />
+  <div class="container">
+   <form method="POST" id="comment_form">
+    <div class="form-group">
+     <textarea name="comment_blog" id="comment_blog" class="form-control" placeholder="Write blog here" rows="10"></textarea>
+    </div>
+    <div class="form-group">
+     <input type="text" name="comment_name" id="comment_name" class="form-control" placeholder="Enter Name" />
+    </div>
+    <div class="form-group">
+     <textarea name="comment_content" id="comment_content" class="form-control" placeholder="Write a comment" rows="5"></textarea>
+    </div>
+    <div class="form-group">
+     <input type="hidden" name="comment_id" id="comment_id" value="0" />
+     <input type="submit" name="submit" id="submit" class="btn btn-info" value="Submit" />
+    </div>
+   </form>
+   <span id="comment_message"></span>
+   <br />
+   <div id="display_comment"></div>
+  </div>
+ </body>
+</html>
+
+<script>
+$(document).ready(function(){
+ 
+ $('#comment_form').on('submit', function(event){
+  event.preventDefault();
+  var form_data = $(this).serialize();
+  $.ajax({
+   url:"add_comment.php",
+   method:"POST",
+   data:form_data,
+   dataType:"JSON",
+   success:function(data)
+   {
+    if(data.error != '')
+    {
+     $('#comment_form')[0].reset();
+     $('#comment_message').html(data.error);
+     $('#comment_id').val('0');
+     load_comment();
+    }
+   }
+  })
+ });
+
+ load_comment();
+
+ function load_comment()
+ {
+  $.ajax({
+   url:"fetch_comment.php",
+   method:"POST",
+   success:function(data)
+   {
+    $('#display_comment').html(data);
+   }
+  })
+ }
+
+ $(document).on('click', '.reply', function(){
+  var comment_id = $(this).attr("id");
+  $('#comment_id').val(comment_id);
+  $('#comment_name').focus();
+ });
+ 
+});
+</script>
